044e90f6e8ebcd2067254b4af53e85c177dc3251
Anastasiia Kozlova <anautomnee@gmail.com>
2024-08-01 21:43:30 +0200

    hw10

---
+++ src/components/App.jsx
@@ -0,0 +1,12 @@
+import { CatImage } from "./CatImage";
+import s from "./App.module.css";
+
+function App() {
+  return (
+    <div className={s.container}>
+      <CatImage />
+    </div>
+  );
+}
+
+export default App;
